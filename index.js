06bcbd8de1ecffaf3a3672ef795d82a9d0dda1f7
Priyanshu Tiwari <priyanshu.tiwari@neosoftmail.com>
2025-03-18 09:02:37 +0530

    created function to get local network Ip address in server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config'
+import os from "os";
 import connectDB from "./db/index.js";
 import { Student } from "./models/student.model.js";
 import { Transaction } from "./models/transaction.model.js";
@@ -11,6 +12,19 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// Function to get the local IP address
+const getLocalIp = () => {
+    const interfaces = os.networkInterfaces();
+    for (const interfaceName in interfaces) {
+        for (const iface of interfaces[interfaceName]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return '0.0.0.0'; // Default to all network interfaces
+};
 
 
 app.post('/byENR', async (req, res) => {
@@ -115,8 +129,9 @@ const transformStudentData = (apiResponses) => {
 
 // Connect MongoDB and Start Server
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("Server is running on Port: " + PORT);
+    const ipAddress = getLocalIp();
+    app.listen(PORT, ipAddress, () => {
+        console.log(`Server is running on http://${ipAddress}:${PORT}`);
     });
 }).catch((error) => {
     console.log("MongoDB Connection Failed !!!", error);
